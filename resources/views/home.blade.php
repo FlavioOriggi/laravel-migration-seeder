1823e4f63d6a1c36dbe58d8dffb0f17d3c3e3fff
Flavio <flavio.origgi@outlook.it>
2021-09-08 23:33:03 +0200

    esce a video l'elenco completo, da sistemare bootsrap che non funziona

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Our Trips')
+    
+@section('content')
+
+    <div class="container">
+        @foreach ($trips as $trip)
+        {{-- @dd($trip) --}}
+            <div class="card">
+                <h5 class="card-header">{{ $trip->title}}</h5>
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $trip->nation}}</h5>
+                        <p class="card-text">{{ $trip->description}}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+            
+        @endforeach
+        
+    </div>
+
+
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p class="lead"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, vel sit? Iusto, cum provident? Ducimus fugiat illo veniam alias sint dolorem dolore veritatis! Commodi maxime corrupti quos cupiditate quis quas?</p>
+</footer>
